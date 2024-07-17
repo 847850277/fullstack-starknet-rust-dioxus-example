15a7e54dc6eb45b624cc285d5a32928b2ee313cb
zhengpeng <847850277@qq.com>
2024-07-17 15:23:37 +0800

    feat: use_resource demo.

--- src/components/eth/mod.rs
+++ src/components/eth/mod.rs
@@ -6,25 +6,36 @@ use crate::services::eth::*;
 #[component]
 pub fn Eth() -> Element {
 
-    let mut count = use_signal(|| 0);
-    let mut text = use_signal(|| "...".to_string());
-    let server_future = use_server_future(get_server_data)?;
+    // let mut count = use_signal(|| 0);
+    // let mut text = use_signal(|| "...".to_string());
+    // let server_future = use_server_future(get_server_data)?;
+    //
+    // rsx! {
+    //     h1 { "High-Five counter: {count}" }
+    //     button { onclick: move |_| count += 1, "Up high!" }
+    //     button { onclick: move |_| count -= 1, "Down low!" }
+    //     button {
+    //         onclick: move |_| async move {
+    //             if let Ok(data) = get_server_data().await {
+    //                 println!("Client received: {}", data);
+    //                 text.set(data.clone());
+    //                 post_server_data(data).await.unwrap();
+    //             }
+    //         },
+    //         "Run a server function!"
+    //     }
+    //     "Server said: {text}"
+    // }
 
-    rsx! {
-        h1 { "High-Five counter: {count}" }
-        button { onclick: move |_| count += 1, "Up high!" }
-        button { onclick: move |_| count -= 1, "Down low!" }
-        button {
-            onclick: move |_| async move {
-                if let Ok(data) = get_server_data().await {
-                    println!("Client received: {}", data);
-                    text.set(data.clone());
-                    post_server_data(data).await.unwrap();
-                }
-            },
-            "Run a server function!"
-        }
-        "Server said: {text}"
+    let mut resource = use_resource(move || async move {
+        // This will run every time the revision signal changes because we read the count inside the future
+        //reqwest::get("https://github.com/DioxusLabs/awesome-dioxus/blob/1d03b42/awesome.json").await
+        get_server_data().await
+    });
+    match &*resource.read_unchecked() {
+        Some(Ok(value)) => rsx! { "{value:?}" },
+        Some(Err(err)) => rsx! { "Error: {err}" },
+        None => rsx! { "Loading..." },
     }
 
 }

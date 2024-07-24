e5c9b4ae72614786985fe6b5b0bc20e898593667
zhengpeng <847850277@qq.com>
2024-07-24 16:42:21 +0800

    feat: build error

--- src/my_starknet/contract/mod.rs
+++ src/my_starknet/contract/mod.rs
@@ -1,19 +1,18 @@
 use provider::create_jsonrpc_client;
 use starknet::core::types::{BlockId, BlockTag, FieldElement, FunctionCall};
+use starknet::core::utils::get_selector_from_name;
 use starknet::providers::{JsonRpcClient, Provider};
 use starknet::providers::jsonrpc::HttpTransport;
-use tracing::log::info;
 
 use crate::my_starknet::provider;
 
-pub async fn call_contract_read_function(provider: JsonRpcClient<HttpTransport>, contract_address: FieldElement, selector: FieldElement, call_data: Vec<FieldElement>) -> Vec<FieldElement> {
-    info!("call_contract_read_function contract_address: {}, selector: {}, call_data: {:?}", contract_address, selector, call_data);
+pub async fn call_contract_read_function(provider: JsonRpcClient<HttpTransport>, contract_address: FieldElement, selector: String, call_data: Vec<FieldElement>) -> Vec<FieldElement> {
     let call_result =
         provider
             .call(
                 FunctionCall {
                     contract_address: contract_address,
-                    entry_point_selector: selector,
+                    entry_point_selector: get_selector_from_name(&selector).unwrap(),
                     calldata: call_data,
                 },
                 BlockId::Tag(BlockTag::Latest),
--- src/services/eth/mod.rs
+++ src/services/eth/mod.rs
@@ -4,7 +4,6 @@ use reqwest::Error;
 use serde_json::json;
 use tracing::info;
 use starknet::core::types::FieldElement;
-use hex;
 use crate::my_starknet::provider::{create_jsonrpc_client, Network};
 use crate::my_starknet::contract::call_contract_read_function;
 
@@ -28,11 +27,8 @@ pub async fn call_read_function(my_selector: String, contract_address: String) -
     let contract_address = FieldElement::from_hex_be(&contract_address)
         .map_err(|err| ServerFnError::new(err.to_string()))?;
     // change my_select string to hex in rust
-    let hex_string = hex::encode(my_selector);
-    let selector = FieldElement::from_hex_be(&hex_string)
-        .map_err(|err| ServerFnError::new(err.to_string()))?;
 
-    let response = call_contract_read_function(create_jsonrpc_client(Network::Testnet), contract_address, selector,vec![]).await;
+    let response = call_contract_read_function(create_jsonrpc_client(Network::Testnet), contract_address, my_selector,vec![]).await;
     dbg!(response);
 
     return Ok(Contract{
